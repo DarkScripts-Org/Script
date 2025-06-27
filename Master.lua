5a52922bbd02e45639ce27bd3aad3883711e4e4b
DarkScripts-Org <muzammilgujjar14567@gmail.com>
2025-06-27 17:06:52 +0500

    Create Master.lua

---
+++ Master.lua
@@ -0,0 +1,5 @@
+-- Script 1: $mileScript
+loadstring(game:HttpGet("https://raw.githubusercontent.com/DarkScripts-Org/Script/refs/heads/main/%24mileScript.lua"))()
+
+-- Script 2: Dark Script
+loadstring(game:HttpGet("https://raw.githubusercontent.com/DarkScripts-Org/Script/refs/heads/main/2098ebd0d62f7acd7660dcb26c7e8d1b.txt"))()
